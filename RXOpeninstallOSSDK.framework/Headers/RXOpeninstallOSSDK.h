d04c44a9ca411f1c04cf193a552a571d2906d6b3
chenhan <chenhan@jixiang.cn>
2026-01-06 11:13:09 +0800

    3.8.0

---
+++ RXOpeninstallOSSDK.framework/Headers/RXOpeninstallOSSDK.h
@@ -0,0 +1,18 @@
+//
+//  RXOpeninstallOSSDK.h
+//  RXOpeninstallOSSDK
+//
+//  Created by 陈汉 on 2026/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RXOpeninstallOSSDK.
+FOUNDATION_EXPORT double RXOpeninstallOSSDKVersionNumber;
+
+//! Project version string for RXOpeninstallOSSDK.
+FOUNDATION_EXPORT const unsigned char RXOpeninstallOSSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RXOpeninstallOSSDK/PublicHeader.h>
+
+#import <RXOpeninstallOSSDK/RXOpeninstallService.h>
